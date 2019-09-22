5f1598d7134b9b98270dbce4cb342c20a52abe13
Dennis Dang <dangggdennis@gmail.com>
2019-09-22 17:10:54 -0400

    checkout view with alert

--- iDine/CheckoutView.swift
+++ iDine/CheckoutView.swift
@@ -9,13 +9,21 @@
 import SwiftUI
 
 struct CheckoutView: View {
+    @EnvironmentObject var order: Order
     static let paymentTypes = ["Cash", "Credit Card", "iDine Points" ]
     static let tipAmounts = [10, 15, 20, 25, 0]
+    
+    var totalPrice: Double {
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(Self.tipAmounts[tipAmount])
+        return total + tipValue
+    }
     
     @State private var tipAmount = 1
     @State private var addLoyaltyDetails = false
     @State private var loyaltyNumber = ""
     @State private var paymentType = 0
+    @State private var showingPaymentAlert = false
     
     var body: some View {
         Form {
@@ -35,19 +43,23 @@ struct CheckoutView: View {
             
             Section(header: Text("Add a tip?")) {
                 Picker("Percentage:", selection: $tipAmount) {
-                    ForEach(0 ..< Self.tipAmounts.count) {
+                    ForEach(0 ..< Self.tipAmounts.count, id: \.self) {
                         Text("\(Self.tipAmounts[$0])%")
                     }
                 }.pickerStyle(SegmentedPickerStyle())
             }
             
             Section(header:
-                Text("TOTAL: $100")
+                Text("TOTAL: $\(self.totalPrice, specifier: "%.2f")")
+                    .font(.largeTitle)
             ) {
                 Button("Confirm order") {
-                    // place the order
+                    self.showingPaymentAlert.toggle()
                 }
             }
+        }
+        .alert(isPresented: $showingPaymentAlert) {
+            Alert(title: Text("Order confirmed"), message: Text("Your total was $\(totalPrice, specifier: "%.2f") – thank you!"), dismissButton: .default(Text("OK")))
         }
         .navigationBarTitle(Text("Payment"), displayMode: .inline)
     }
